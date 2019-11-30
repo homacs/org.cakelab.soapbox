cfb4d94701f2fc67dfe188388244cbb7cd77c86c
homac <homac@strace.org>
2019-11-30 11:20:43 +0100

    minor changes

--- src/org/cakelab/oge/app/ApplicationBase.java
+++ src/org/cakelab/oge/app/ApplicationBase.java
@@ -57,10 +57,10 @@ public abstract class ApplicationBase extends AbstractAplicationBase {
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 		glfwWindowHint(GLFW_SAMPLES, info.settings.samples);
-		glfwWindowHint(GLFW_STEREO, info.settings.stereo ? GL_TRUE : GL_FALSE);
+		glfwWindowHint(GLFW_STEREO,  info.settings.stereo ? GL_TRUE : GL_FALSE);
 
-		glfwWindowHint(GLFW_ALPHA_BITS, 16);
-		glfwWindowHint(GLFW_DEPTH_BITS, 32);
+		glfwWindowHint(GLFW_ALPHA_BITS,  16);
+		glfwWindowHint(GLFW_DEPTH_BITS,  32);
 		glfwWindowHint(GLFW_STENCIL_BITS, 0);
 		long monitor = 0;
 		if (info.settings.fullscreen) {
